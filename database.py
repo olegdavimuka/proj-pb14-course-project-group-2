018944eb37157f9efd46a490474f0df7094bd4a0
tnstsmsk <omelkaramelka@gmail.com>
2023-11-20 21:15:30 +0200

    #3 PostgreSQL and #4 SQLAlchemy

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+db_config = config['postgresql']
+
+DATABASE_URI = f"postgresql+psycopg2://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+
+engine = create_engine(DATABASE_URI)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
