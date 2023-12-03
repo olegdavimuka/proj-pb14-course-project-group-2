c56d606baebc2a3401cbc9abfec9801a8d0786ef
Olena Yanishevska <olena.yanishevska.pz.2021@lpnu.ua>
2023-12-03 14:28:41 +0200

    Delete first migration

--- app/migrations/versions/c65392f24428_create_tables.py
+++ app/migrations/versions/c65392f24428_create_tables.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "c65392f24428"
-down_revision: Union[str, None] = "0975a7a12f09"
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
