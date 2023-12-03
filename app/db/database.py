59c73bd3eced4336fcf41afbb349c24cfd7db390
Olena Yanishevska <olena.yanishevska.pz.2021@lpnu.ua>
2023-12-03 10:53:41 +0200

    Fix migrations and telegram api

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,6 @@
 import configparser
 
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -18,3 +18,4 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
+metadata = MetaData()
